447bea9ebddcd3f71636a7075fc2d10cdebba32f
Mike Jacobson <michaeljjacobson1@gmail.com>
2021-08-14 14:17:28 -0700

    Add initial version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # primeng-calendar-mode-toggle-directive
 Angular 11 Directive that adds Selection Mode toggle to PrimeNG Calendar component
+
+Worklist StackBlitz:
+
+https://stackblitz.com/edit/primeng-calendar-demo-with-selectionmode-toggle-directive?file=src%2Fapp%2Fcalendar-mode-toggle.directive.ts
+
+It just toggles between 'range' mode and 'single' mode but feel free to take the code and enhance it however you want.
+
+And please don't submit any Issues. I'm sure it has issues. This was just a fun POC for my own learning and amusement that I thought I'd share, and put somewhere where I can get to it later if I ever need it.
---
+++ src/calendar-mode-toggle.directive.ts
@@ -0,0 +1,72 @@
+import { Directive, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { Calendar } from 'primeng/calendar';
+
+const defaultSelectionMode = 'single';
+
+@Directive({
+  selector: '[calendarModeToggle]'
+})
+export class CalendarModeToggleDirective implements OnInit, OnDestroy {
+  mode: string;
+  toggleButton: HTMLButtonElement;
+  stopListening;
+
+  constructor(
+    private el: ElementRef,
+    private calendar: Calendar,
+    private rn: Renderer2
+  ) { }
+
+  ngOnInit() {
+    this.mode = this.calendar.selectionMode || defaultSelectionMode;
+
+    this.calendar.onShow.subscribe(() => {
+      this.addToggleButtonToButtonBar();
+    });
+
+    this.calendar.onClose.subscribe(() => {
+      this.stopListening();
+      this.stopListening = null;
+      this.toggleButton = null;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.stopListening) {
+      this.stopListening();
+    }
+  }
+
+  toggleMode() {
+    this.mode = this.mode === 'range' ? 'single' : 'range';
+    this.calendar.selectionMode = this.mode;
+    this.calendar.writeValue(null);
+    this.setButtonLabel();
+  }
+
+  addToggleButtonToButtonBar() {
+    const buttonBar = this.el.nativeElement.querySelector('.p-datepicker-buttonbar');
+    const lastButton = buttonBar.children[1];
+    const toggleButton = this.rn.createElement('button');
+    this.rn.setAttribute(toggleButton, 'class', 'p-button-text p-ripple p-button p-component');
+    this.rn.setAttribute(toggleButton, 'style', 'font-size: smaller');
+
+    this.stopListening = this.rn.listen(toggleButton, 'click', () => this.toggleMode());
+    this.rn.insertBefore(buttonBar, toggleButton, lastButton);
+
+    this.toggleButton = toggleButton;
+    this.setButtonLabel();
+  }
+
+  setButtonLabel() {
+    this.toggleButton.innerText = this.mode === 'single' ? this.buttonTextSingle : this.buttonTextRange;
+  }
+
+  buttonTextSingle = `  Selection Mode Single
+  Change to Range
+  `;
+
+  buttonTextRange = `  Selection Mode Range
+  Change to Single
+  `
+}
